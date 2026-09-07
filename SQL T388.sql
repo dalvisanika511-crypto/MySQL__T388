6431d9012ec8701d391ba94c2954bfe9b020eaba
Sanika Dalvi <dalvisanika511@gmail.com>
2026-09-07 11:09:56 +0530

    sql file upload

---
+++ SQL T388.sql
@@ -0,0 +1,69 @@
+use t388;
+show tables;
+select * from employee;
+delete from employee where gender ="Male";
+delete from employee where age>25;
+INSERT INTO Employee values
+(1001,"John Doe","IT",35000,"Male",25),
+(1002, 'Mary Smith', 'HR', 45000, 'Female', 27),
+(1003, 'James Brown', 'Finance', 50000, 'Male', 28),
+(1004, 'Mike Walker', 'Finance', 50000, 'Male', 28),
+(1005, 'Linda Jones', 'HR', 75000, 'Female', 26),
+(1006, 'Anurag Mohanty', 'IT', 35000, 'Male', 25),
+(1007, 'Priyanka Dewangan', 'HR', 45000, 'Female', 27),
+(1008, 'Sambit Mohanty', 'IT', 50000, 'Male', 28),
+(1009, 'Pranaya Kumar', 'IT', 50000, 'Male', 28),
+(1010, 'Hina Sharma', 'HR', 75000, 'Female', 26);
+truncate table employee;
+alter table employee 
+add Location varchar(10);
+alter table employee 
+add Bonus float after salary;
+select * from employee;
+alter table employee 
+add Title Varchar(5) first;
+desc employee;
+alter table employee modify fullname varchar(35);
+alter table employee change column location address varchar(36);
+INSERT INTO Employee values
+(1001,"John Doe","IT",35000,"Male",25),
+(1002, 'Mary Smith', 'HR', 45000, 'Female', 27),
+(1003, 'James Brown', 'Finance', 50000, 'Male', 28),
+(1004, 'Mike Walker', 'Finance', 50000, 'Male', 28),
+(1005, 'Linda Jones', 'HR', 75000, 'Female', 26),
+(1006, 'Anurag Mohanty', 'IT', 35000, 'Male', 25),
+(1007, 'Priyanka Dewangan', 'HR', 45000, 'Female', 27),
+(1008, 'Sambit Mohanty', 'IT', 50000, 'Male', 28),
+(1009, 'Pranaya Kumar', 'IT', 50000, 'Male', 28),
+(1010, 'Hina Sharma', 'HR', 75000, 'Female', 26);
+update employee set address ="Thane";
+alter table employee drop title, drop bonus, drop address;
+INSERT INTO Employee values
+(1001,"John Doe","IT",35000,"Male",25),
+(1002, 'Mary Smith', 'HR', 45000, 'Female', 27),
+(1003, 'James Brown', 'Finance', 50000, 'Male', 28),
+(1004, 'Mike Walker', 'Finance', 50000, 'Male', 28),
+(1005, 'Linda Jones', 'HR', 75000, 'Female', 26),
+(1006, 'Anurag Mohanty', 'IT', 35000, 'Male', 25),
+(1007, 'Priyanka Dewangan', 'HR', 45000, 'Female', 27),
+(1008, 'Sambit Mohanty', 'IT', 50000, 'Male', 28),
+(1009, 'Pranaya Kumar', 'IT', 50000, 'Male', 28),
+(1010, 'Hina Sharma', 'HR', 75000, 'Female', 26);
+alter table employee 
+add Location varchar(10);
+alter table employee 
+add Bonus float after salary;
+alter table employee 
+add Location varchar(10);
+select * from employee;
+alter table employee modify fullname varchar(35);
+alter table employee change column location address varchar(36);
+alter table employee 
+add Title Varchar(5) first;
+update employee set address ="Thane";
+update employee set address ="Dombivali"
+where department ="IT";
+UPDATE employee set Title="Mr" where gender= "Male";
+UPDATE employee set Title="Mrs" where gender= "Female";
+select * from employee;
+update employee set bonus=salary*0.05;
